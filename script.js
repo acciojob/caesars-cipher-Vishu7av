d401d605b52b4851227d2a1d5bbf6b35e5814b31
Vishnu Kumar <77917126+Vishu7av@users.noreply.github.com>
2023-03-15 00:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Your Script here.
+function rot13(str) { // LBH QVQ VG!
+  
+  var string = "";
+  for(var i = 0; i < str.length; i++) {
+    var temp = str.charAt(i);
+    if(temp !== " " || temp!== "!" || temp!== "?") {
+       string += String.fromCharCode(13 + String.prototype.charCodeAt(temp));
+    } else {
+      string += temp;
+    }
+  }
+  
+  return string;
+}
+
+// Change the inputs below to test
+console.log(rot13("SERR PBQR PNZC"));
